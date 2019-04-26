804e63bd18c794849393befe8222e3cc7ba608c4
Denis Mondon <mondon.denis@gmail.com>
2019-04-26 23:39:23 +0200

    added left, up, right, down arrow animations

--- demo/src/main/java/com/blunderer/easyanimatedvectordrawabledemo/MainActivity.kt
+++ demo/src/main/java/com/blunderer/easyanimatedvectordrawabledemo/MainActivity.kt
@@ -15,6 +15,11 @@ class MainActivity : AppCompatActivity() {
         activity_main_play_button.setOnClickListener { EasyAnimatedVectorDrawable.setImageType(activity_main_imageview, EasyAnimatedVectorDrawable.Type.PLAY, Color.RED) }
         activity_main_pause_button.setOnClickListener { EasyAnimatedVectorDrawable.setImageType(activity_main_imageview, EasyAnimatedVectorDrawable.Type.PAUSE, Color.RED) }
         activity_main_stop_button.setOnClickListener { EasyAnimatedVectorDrawable.setImageType(activity_main_imageview, EasyAnimatedVectorDrawable.Type.STOP, Color.RED) }
+
+        activity_main_left_arrow_button.setOnClickListener { EasyAnimatedVectorDrawable.setImageType(activity_main_imageview, EasyAnimatedVectorDrawable.Type.LEFT_ARROW, Color.RED) }
+        activity_main_up_arrow_button.setOnClickListener { EasyAnimatedVectorDrawable.setImageType(activity_main_imageview, EasyAnimatedVectorDrawable.Type.UP_ARROW, Color.RED) }
+        activity_main_right_arrow_button.setOnClickListener { EasyAnimatedVectorDrawable.setImageType(activity_main_imageview, EasyAnimatedVectorDrawable.Type.RIGHT_ARROW, Color.RED) }
+        activity_main_down_arrow_button.setOnClickListener { EasyAnimatedVectorDrawable.setImageType(activity_main_imageview, EasyAnimatedVectorDrawable.Type.DOWN_ARROW, Color.RED) }
     }
 
 }
--- library/src/main/java/com/blunderer/easyanimatedvectordrawable/EasyAnimatedVectorDrawable.kt
+++ library/src/main/java/com/blunderer/easyanimatedvectordrawable/EasyAnimatedVectorDrawable.kt
@@ -30,7 +30,7 @@ object EasyAnimatedVectorDrawable {
             return
         }
 
-        val animatedVectorDrawable = getAnimatedVectorDrawable(currentTypeTag, type) ?: return
+        val animatedVectorDrawable = getAnimatedVectorDrawable(currentTypeTag, type) ?: 0
         if (animatedVectorDrawable != 0) {
             setImageDrawable(imageView, animatedVectorDrawable, type, tintColor)
             return
@@ -92,19 +92,40 @@ object EasyAnimatedVectorDrawable {
         val pauseDrawableType = AnimatedVectorDrawableConfig.DrawableType(Type.PAUSE, R.drawable.eavd_vd_pause)
         val stopDrawableType = AnimatedVectorDrawableConfig.DrawableType(Type.STOP, R.drawable.eavd_vd_stop)
 
+        val leftArrowDrawableType = AnimatedVectorDrawableConfig.DrawableType(Type.LEFT_ARROW, R.drawable.eavd_vd_left_arrow)
+        val upArrowDrawableType = AnimatedVectorDrawableConfig.DrawableType(Type.UP_ARROW, R.drawable.eavd_vd_up_arrow)
+        val rightDrawableType = AnimatedVectorDrawableConfig.DrawableType(Type.RIGHT_ARROW, R.drawable.eavd_vd_right_arrow)
+        val downDrawableType = AnimatedVectorDrawableConfig.DrawableType(Type.DOWN_ARROW, R.drawable.eavd_vd_down_arrow)
+
         return AnimatedVectorDrawableConfig.Builder()
+                // Play, Pause, Stop
                 .addAnimatedVectorDrawable(playDrawableType, pauseDrawableType, R.drawable.avd_play_to_pause)
                 .addAnimatedVectorDrawable(playDrawableType, stopDrawableType, R.drawable.avd_play_to_stop)
                 .addAnimatedVectorDrawable(pauseDrawableType, playDrawableType, R.drawable.avd_pause_to_play)
                 .addAnimatedVectorDrawable(pauseDrawableType, stopDrawableType, R.drawable.avd_pause_to_stop)
                 .addAnimatedVectorDrawable(stopDrawableType, playDrawableType, R.drawable.avd_stop_to_play)
                 .addAnimatedVectorDrawable(stopDrawableType, pauseDrawableType, R.drawable.avd_stop_to_pause)
+
+                // Left arrow, Top arrow, Right arrow, Bottom arrow
+                .addAnimatedVectorDrawable(leftArrowDrawableType, upArrowDrawableType, R.drawable.avd_left_arrow_to_up_arrow)
+                .addAnimatedVectorDrawable(leftArrowDrawableType, rightDrawableType, R.drawable.avd_left_arrow_to_right_arrow)
+                .addAnimatedVectorDrawable(leftArrowDrawableType, downDrawableType, R.drawable.avd_left_arrow_to_down_arrow)
+                .addAnimatedVectorDrawable(upArrowDrawableType, rightDrawableType, R.drawable.avd_up_arrow_to_right_arrow)
+                .addAnimatedVectorDrawable(upArrowDrawableType, downDrawableType, R.drawable.avd_up_arrow_to_down_arrow)
+                .addAnimatedVectorDrawable(upArrowDrawableType, leftArrowDrawableType, R.drawable.avd_up_arrow_to_left_arrow)
+                .addAnimatedVectorDrawable(rightDrawableType, downDrawableType, R.drawable.avd_right_arrow_to_down_arrow)
+                .addAnimatedVectorDrawable(rightDrawableType, leftArrowDrawableType, R.drawable.avd_right_arrow_to_left_arrow)
+                .addAnimatedVectorDrawable(rightDrawableType, upArrowDrawableType, R.drawable.avd_right_arrow_to_up_arrow)
+                .addAnimatedVectorDrawable(downDrawableType, leftArrowDrawableType, R.drawable.avd_down_arrow_to_left_arrow)
+                .addAnimatedVectorDrawable(downDrawableType, upArrowDrawableType, R.drawable.avd_down_arrow_to_up_arrow)
+                .addAnimatedVectorDrawable(downDrawableType, rightDrawableType, R.drawable.avd_down_arrow_to_right_arrow)
                 .build()
     }
 
     enum class Type {
 
-        PLAY, PAUSE, STOP
+        PLAY, PAUSE, STOP,
+        LEFT_ARROW, UP_ARROW, RIGHT_ARROW, DOWN_ARROW
 
     }
 
